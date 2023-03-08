046bfa7e9fe7297e9654218a5d1e79360a84a5d0
Serdar Mammadov <57108059+serdarm09@users.noreply.github.com>
2023-03-08 14:00:25 +0300

    For-while-#3

---
+++ donguler-for-while/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int i;
+        int j=0;
+        //for
+        for(i=0;i<10;i++)
+          {
+          System.out.println(i);
+          }
+        System.out.println("For dongusu bitmistir");
+        //while
+        while(j<10)
+          {
+              System.out.println(j);
+              j++;
+          }
+        System.out.println("While dongusu bitimisti");
+
+    }
+}
